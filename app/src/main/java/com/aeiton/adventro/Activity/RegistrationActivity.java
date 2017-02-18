9078c6f733327efbaa1e313992b3e73b18ad51b0
haxzie <musthu.gm@gmail.com>
2017-02-18 12:43:03 +0530

    build gradle

--- app/src/main/java/com/aeiton/adventro/Activity/RegistrationActivity.java
+++ app/src/main/java/com/aeiton/adventro/Activity/RegistrationActivity.java
@@ -3,6 +3,8 @@ package com.aeiton.adventro.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.aeiton.adventro.R;
+
 public class RegistrationActivity extends AppCompatActivity {
 
     @Override
