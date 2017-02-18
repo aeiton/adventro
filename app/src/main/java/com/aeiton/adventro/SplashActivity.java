fa0c8cd9b256a3a70a569e679e6f14c922160d83
Rumaan <rumaankalander@gmail.com>
2017-02-18 12:29:54 +0530

    added Digits

--- app/src/main/java/com/aeiton/adventro/SplashActivity.java
+++ app/src/main/java/com/aeiton/adventro/SplashActivity.java
@@ -2,12 +2,23 @@ package com.aeiton.adventro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.digits.sdk.android.Digits;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import io.fabric.sdk.android.Fabric;
 
 public class SplashActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "qLqbl3IixfIPrX6CjFKwzkwDk";
+    private static final String TWITTER_SECRET = "NzZC5FkGUBzvcvG4CA8nvlh1KmX7x2wJx3ebY7VECiJyd5PffQ";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
         setContentView(R.layout.activity_splash);
 
 
